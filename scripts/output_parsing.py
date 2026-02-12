4c509ae9115778a49d575c4bfc1d269fbd5dea04
Amanda Kartchner <aw998@byu.edu>
2026-02-12 14:26:15 -0700

    recent changes

---
+++ scripts/output_parsing.py
@@ -0,0 +1,19 @@
+import os
+
+## Get file names for parsing
+## Go through output directory
+## Add data from each file
+
+def get_input_file_names():
+    print(f"Current directory: {os.getcwd()}")
+
+    # print(f"input files: {os.listdir('output_files')}")
+
+
+def main():
+    
+
+    get_input_file_names()
+
+if __name__ == "__main__":
+    main()
--- slurm_scripts/run_boltz_test.sh
+++ slurm_scripts/run_boltz_test.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=boltz_cpu
-#SBATCH --output=../logs/boltz_%j.out
-#SBATCH --error=../logs/boltz_%j.err
+#SBATCH --job-name=boltz_h100
+#SBATCH --output=../logs/boltz_%j.log
+#SBATCH --error=../logs/boltz_%j.log
 #SBATCH --time=02:00:00
+#SBATCH --partition=granite-gpu-guest
+#SBATCH --qos=granite-gpu-guest
+#SBATCH --account=stewartp
+#SBATCH --gres=gpu:rtx5000:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 
-module load python
+module purge
 module load boltz2
 
-boltz predict ../../boltz/examples/affinity.yaml \
+nvidia-smi
+
+boltz predict ../../boltz/examples/prot.yaml \
     --use_msa_server \
-    --accelerator cpu \
-    --devices 1 \
     --num_workers 8 \
     --out_dir ../outputs
